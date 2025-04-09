d859395b4956980d7d692662584cd03264610a8e
janrendel <janrendel11@gmail.com>
2025-04-09 11:46:14 +0900

    BookMarket의 5장 상세정보 표시 구현 중 Reository 계층까지 됨

--- src/main/java/kr/ac/kopo/janrendel11/bookmarket/repository/BookRepository.java
+++ src/main/java/kr/ac/kopo/janrendel11/bookmarket/repository/BookRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface BookRepository {
     List<Book> getAllBookList(); // 도서 목록 가져오는 메서드 선언
+    Book getBookById(String bookId) throws IllegalAccessException;
 }
